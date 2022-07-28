48a069b69489bc5ff7f3b65566cdd3bb8fd93ab0
Falllll <faldiharido@gmail.com>
2022-07-29 00:15:13 +0700

    add some changes

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,26 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'as' => 'admin.'
+], function () {
+    Route::group(['middleware' => ['role:admin', 'auth']], function () {
+        route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('index');
+    });
+});
+
+Route::group([
+    'prefix' => 'developer',
+    'namespace' => 'Developer',
+    'as' => 'developer.'
+], function () {
+    Route::group(['middleware' => ['role:developer', 'auth']], function () {
+        route::get('/', [\App\Http\Controllers\Developer\DashboardController::class, 'index'])->name('index');
+    });
+});
+
+
+
 require __DIR__.'/auth.php';
